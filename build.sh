4d050b9d8c9e82119822c4950e3be464f5519cb5
Tat-Chee Wan (USM Signature) <105933+tcwan@users.noreply.github.com>
2026-06-26 09:44:31 +0800

    Updated build.sh for dbgsym generation

--- build.sh
+++ build.sh
@@ -6,7 +6,13 @@ mkdir -p /workspace/out
 export CC=aarch64-linux-gnu-gcc
 export $(dpkg-architecture -aarm64); export CROSS_COMPILE=aarch64-linux-gnu-
 fakeroot debian/rules clean
+
+# To modify Kernel configs, uncomment the editconfigs line
 #fakeroot debian/rules editconfigs
+
+# To generate dbgsyms, set skipdbg=false
+#fakeroot debian/rules binary-headers binary binary-perarch skipdbg=false
 fakeroot debian/rules binary-headers binary binary-perarch
+
 mv ../*.deb /workspace/out/
 echo "Done ;-)"
